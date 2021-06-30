c7df4aa8cf7abb289f978b09c5beb7857c6bc4b2
halustyan <halustyan@ukr.net>
2021-06-30 13:44:22 +0300

    Update and rename act_execute.php to ActionExecute.php

--- src/models/ActionExecute.php
+++ src/models/ActionExecute.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace Htmlacademy\Models;
-use Htmlacademy\Models\ci_action;
+use Htmlacademy\Models\ActionExecute;
 
 class ActionExecute extends AbstractClass
 {
     protected $publicName = "Выполнено";
     protected $innerName = "act_execute";
 
-    public function CheckRights(int $idExecutor, int $idTaskmaker, int $idUser):bool
+    public function checkRights(int $idExecutor, int $idTaskmaker, int $idUser):bool
     {
         return $idExecutor === $idUser;
     }
