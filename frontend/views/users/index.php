3e88cabb2d76531757686422ede39b2261e2e4f2
Maxym Berezynets <halustyan@ukr.net>
2021-10-13 23:35:08 +0300

    deleted not correct

--- frontend/views/users/index.php
+++ frontend/views/users/index.php
@@ -69,76 +69,7 @@
                 
                
 
-             <!--   <div class="content-view__feedback-card user__search-wrapper">
-                    <div class="feedback-card__top">
-                        <div class="user__search-icon">
-                            <a href="#">
-                                
-                            <img src="<?php echo $item->user_img?>" width="65" height="65">
-        
-                        </a>
-                            <span>    <?php echo count($item->activeTasks) ?> заданий</span>
-                            <span><?php echo count($item->activeReplies) ?> отзывов</span>
-                        </div>
-                        <div class="feedback-card__top--name user__search-card">
-                            <p class="link-name"><a href="#" class="link-regular"><?php echo $item->name; ?></a></p>
-
-                            <?php $i = 0; while($i<$item->user_stars):?> 
-                            
-                                <span></span>
-    
-                            <?php $i++; endwhile; ?> 
-
-                            <?php $numberDisabled = 5-$item->user_stars; $b =0; while($b<$numberDisabled):?>
-                              <span class="star-disabled"></span>
-                           <?php $b++; endwhile; ?>
-
-                            <b><?php echo $item->user_rating; ?></b>
-                            <p class="user__search-content">
-
-<?php //var_dump( $item->searchContent);?>
-
-                            </p>
-                            <?php 
-                        
-                        $wasOnSite = (strtotime('now')-strtotime($item->user_was_on_site));
-                        $days = floor($wasOnSite/86400);
-                        $hours = floor(($wasOnSite-$days*86400)/(60 * 60));
-                        $min = floor(($wasOnSite-($days*86400+$hours*3600))/60);
-                     ?>
-
-                     <span class="new-task__time">Был на сайте <?php 
-                      if ($days >1) {
-                         echo $days." дней ";
-                     }
-                     else if($hours>1&&1<$days) {
-                         echo $hours." часов ";
-                     }
-                 elseif ($min<60&&1<$hours) {
-                         echo $min." минут ";
-                     }
-                   
-                    
-                     
-                     
-                     ?>  назад</span>
-                        </div>
-                        <div class="link-specialization user__search-link--bottom">
-                        <?php foreach($item->getTagsArray() as $tagsItem):?>
-                          <a href="#" class="link-regular"><?php echo ($tagsItem.", ");?></a>
-                    <?php endforeach;?>
-                        
            
-                    </div>
-
-                       
-                    </div>
-                    <div class="link-specialization user__search-link--bottom">
-         
-                        <a href="#" class="link-regular"></a>
-                   
-                    </div>
-                </div>-->
 
 
 
